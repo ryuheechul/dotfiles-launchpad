50bf5bdfe3feca6afad101e2d782481e637f53dc
Heechul Ryu <ryuhcii@gmail.com>
2021-03-26 00:39:07 +0000

    Search nix.sh to source in ~/.nix-profile too

--- bin/inject-source-nix-path.sh
+++ bin/inject-source-nix-path.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
 echo 'if [ -e /usr/local/etc/profile.d/nix.sh ]; then . /usr/local/etc/profile.d/nix.sh; fi' >> ~/.zshrc
+echo 'if [ -e ~/.nix-profile/etc/profile.d/nix.sh ]; then . ~/.nix-profile/etc/profile.d/nix.sh; fi' >> ~/.zshrc
